af1cc6ca6520b723bfc7f806feca058ce6058ab7
JuliusRamos <juliuscervantesramos@gmail.com>
2024-03-12 23:53:08 +0800

    a

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -126,54 +126,52 @@
 </section> 
 
 <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
-    <div class="container">
+   
 
-        <!-- MENU LINKS -->
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <a href="{{url('/dashboard')}}" class="navbar-brand">MojiVet</a>
-                <li><a href="#home" class="smoothScroll">Home</a></li>
-                <li><a href="#about" class="smoothScroll">About</a></li>
-                <li><a href="#menu" class="smoothScroll">Photos</a></li>
-                <li><a href="{{ route('appointment') }}" class="section-btn btn btn-default smoothScroll">Get appointment</a></li>
-                <li>
-                    <div class="hidden sm:flex sm:items-center sm:ms-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                    <div>{{ Auth::user()->name }}</div>
+    <!-- MENU LINKS -->
+    
+         <ul class="nav navbar-nav navbar-right">
+           <a href="{{url('/dashboard')}}" class="navbar-brand">MojiVet</a>
+              <li><a href="#home" class="smoothScroll">Home</a></li>
+              <li><a href="#about" class="smoothScroll">About</a></li>
+              <li><a href="#menu" class="smoothScroll">Photos</a></li>
+              <li><a href="{{ route('appointment') }}" class="section-btn btn btn-default smoothScroll">Get appointment</a></li>
+              <div class="hidden sm:flex sm:items-center sm:ms-6">
+               <x-dropdown align="right" width="48">
+                   <x-slot name="trigger">
+                       <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                           <div>{{ Auth::user()->name }}</div>
 
-                                    <div class="ms-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
+                           <div class="ms-1">
+                               <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                   <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                               </svg>
+                           </div>
+                       </button>
+                   </x-slot>
 
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('profile.edit')">
-                                    {{ __('Profile') }}
-                                </x-dropdown-link>
+                   <x-slot name="content">
+                       <x-dropdown-link :href="route('profile.edit')">
+                           {{ __('Profile') }}
+                       </x-dropdown-link>
 
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
+                       <!-- Authentication -->
+                       <form method="POST" action="{{ route('logout') }}">
+                           @csrf
 
-                                    <x-dropdown-link :href="route('logout')"
-                                        onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-                </li>
-            </ul>
-        </div>
-
+                           <x-dropdown-link :href="route('logout')"
+                                   onclick="event.preventDefault();
+                                               this.closest('form').submit();">
+                               {{ __('Log Out') }}
+                           </x-dropdown-link>
+                       </form>
+                   </x-slot>
+               </x-dropdown>
+           </div>
+         </ul>
     </div>
+</div>
+
 </section>
 
 
